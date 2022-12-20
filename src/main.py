b66c61fd5a48229c7d705f30dc56d8a7c38d643b
Adam O'Brien <74919798+Adam0Brien@users.noreply.github.com>
2022-12-20 23:55:28 +0000

    Update main.py
    script that runs heartbeat.py,sound,py and bme680data.py all concurrently

--- src/main.py
+++ src/main.py
@@ -5,19 +5,15 @@ def run_script(script_path):
     # Use subprocess to run the script
     subprocess.run(["python3", script_path])
 
-# Create a list of the scripts that you want to run
-scripts = ["heartbeat.py", "sound.py","tempH.py"]
+scripts = ["heartbeat.py", "sound.py","bme680data.py"]
 
-# Create a list of processes
 processes = []
 
-# Start a process for each script
 for script in scripts:
     p = multiprocessing.Process(target=run_script, args=(script,))
     p.start()
     processes.append(p)
 
-# Wait for all processes to complete
 for p in processes:
     p.join()
 
